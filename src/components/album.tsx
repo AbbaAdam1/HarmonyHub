10917f1165022438606c342b74f83e39e18ad67f
AbbaAdam1 <sonicdx15@gmail.com>
2023-07-13 22:45:31 -0400

    test

--- src/components/album.tsx
+++ src/components/album.tsx
@@ -1,7 +1,7 @@
 const Album = ({ togglePlayPause }: { togglePlayPause: () => void }) => (
   <div className="container flex mx-auto w-full items-center">
     <ul className="flex flex-col bg-gray-300 w-screen p-4">
-      <li className="border-gray-400 flex flex-row mb-2">
+      <li className="border-red-400 flex flex-row mb-2">
         <div onClick={togglePlayPause} className="select-none cursor-pointer bg-orange-500 rounded-md flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
           <div className="flex flex-col rounded-md w-10 h-10 bg-gray-300 justify-center items-center mr-4">▷</div>
           <div className="flex-1 pl-1 mr-16">
